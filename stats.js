e616a7e6221304cad74e2ecad23e11069a828db8
DouglasBarbino <douglas-448@hotmail.com>
2016-10-10 11:32:28 -0300

    Update code to obtain data of questions

---
+++ stats.js
@@ -0,0 +1,33 @@
+function sendData(pergunta,correta,fase,respostas,escolhida,acertou){
+	/*var info = {};
+    var path;
+    if(window.location.hostname == "localhost" ){   // for localhost tests
+        path = "/exported-resource/saveStats"
+    }else {                                 // for web version in production, electron and crosswalk versions
+        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
+	}*/
+	/*$.getJSON("remar.json", function(json) {
+        info.exportedResourceId = json.exportedResourceId;
+        info.answer = pergunta;
+        info.question = correta;
+        info.levelId = fase;
+        info.alternatives = respostas;
+        info.choice = escolhida;
+        //info.size = size;
+        info.win = acertou;
+        info.gameType = 'multipleChoice';
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: info,
+            success: function(data) {
+            }
+        })
+});*/
+	console.log(pergunta);
+	console.log(correta);
+	console.log(fase);
+	console.log(respostas);
+	console.log(escolhida);
+	console.log(acertou);
+}
